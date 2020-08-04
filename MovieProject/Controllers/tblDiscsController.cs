49ee6ab6df5e58a45a3119766e88ebccc33737dd
kartiksingh98 <kartik.niku@gmail.com>
2020-08-04 16:05:50 +0530

    Movie

---
+++ MovieProject/Controllers/tblDiscsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieProject;
+
+namespace MovieProject.Controllers
+{
+    public class tblDiscsController : Controller
+    {
+        private dbMovieShopEntities db = new dbMovieShopEntities();
+
+        // GET: tblDiscs
+        public ActionResult Index()
+        {
+            var tblDiscs = db.tblDiscs.Include(t => t.tblMovie);
+            return View(tblDiscs.ToList());
+        }
+
+        // GET: tblDiscs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblDisc tblDisc = db.tblDiscs.Find(id);
+            if (tblDisc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblDisc);
+        }
+
+        // GET: tblDiscs/Create
+        public ActionResult Create()
+        {
+            ViewBag.movie_id = new SelectList(db.tblMovies, "id", "name");
+            return View();
+        }
+
+        // POST: tblDiscs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,movie_id,rent_cost,is_Available")] tblDisc tblDisc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblDiscs.Add(tblDisc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.movie_id = new SelectList(db.tblMovies, "id", "name", tblDisc.movie_id);
+            return View(tblDisc);
+        }
+
+        // GET: tblDiscs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblDisc tblDisc = db.tblDiscs.Find(id);
+            if (tblDisc == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.movie_id = new SelectList(db.tblMovies, "id", "name", tblDisc.movie_id);
+            return View(tblDisc);
+        }
+
+        // POST: tblDiscs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,movie_id,rent_cost,is_Available")] tblDisc tblDisc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblDisc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.movie_id = new SelectList(db.tblMovies, "id", "name", tblDisc.movie_id);
+            return View(tblDisc);
+        }
+
+        // GET: tblDiscs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblDisc tblDisc = db.tblDiscs.Find(id);
+            if (tblDisc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblDisc);
+        }
+
+        // POST: tblDiscs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblDisc tblDisc = db.tblDiscs.Find(id);
+            db.tblDiscs.Remove(tblDisc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieProject/Controllers/tblMoviesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieProject;
+
+namespace MovieProject.Controllers
+{
+    public class tblMoviesController : Controller
+    {
+        private dbMovieShopEntities db = new dbMovieShopEntities();
+
+        // GET: tblMovies
+        public ActionResult Index()
+        {
+            return View(db.tblMovies.ToList());
+        }
+
+        // GET: tblMovies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblMovie tblMovie = db.tblMovies.Find(id);
+            if (tblMovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblMovie);
+        }
+
+        // GET: tblMovies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblMovies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,duration")] tblMovie tblMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblMovies.Add(tblMovie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblMovie);
+        }
+
+        // GET: tblMovies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblMovie tblMovie = db.tblMovies.Find(id);
+            if (tblMovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblMovie);
+        }
+
+        // POST: tblMovies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,duration")] tblMovie tblMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblMovie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblMovie);
+        }
+
+        // GET: tblMovies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblMovie tblMovie = db.tblMovies.Find(id);
+            if (tblMovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblMovie);
+        }
+
+        // POST: tblMovies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblMovie tblMovie = db.tblMovies.Find(id);
+            db.tblMovies.Remove(tblMovie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
